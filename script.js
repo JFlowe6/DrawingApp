c47ddc03292e0f3977c40361807178f9b4950e53
Jack Lowe <jacklowe@live.com>
2022-06-27 18:10:13 +0100

    Added more functions so that drawing is smoother and a clear function to clear the canvas, will comment on the code and upload later

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const canvas = document.getElementById('canvas');
 const increaseBtn = document.getElementById('increase');
 const decreaseBtn = document.getElementById('decrease');
+const clearBtn = document.getElementById('clear');
 const sizeElem = document.getElementById('size');
 const colorElem = document.getElementById('color');
 const ctx = canvas.getContext('2d');
@@ -9,23 +10,34 @@ const ctx = canvas.getContext('2d');
 let size = 10;
 let isPressed=false;
 let color='black';
+let x = undefined;
+let y = undefined;
 
 //this just sees if the mouse is pressed 
 //which will then cause drawing to be displayed
-canvas.addEventListener('mousedown', ()=>{
+canvas.addEventListener('mousedown', (e)=>{
     isPressed = true;
+
+    x = e.offsetX;
+    y = e.offsetY;
 });
 
-canvas.addEventListener('mouseup', ()=>{
+canvas.addEventListener('mouseup', (e)=>{
     isPressed = false;
+
+    x = undefined;
+    y = undefined;
 });
 
 canvas.addEventListener('mousemove', (e)=>{
     if(isPressed){
-        const x = e.offsetX;
-        const y = e.offsetY;
+        const x2 = e.offsetX;
+        const y2 = e.offsetY;
 
-        drawCircle(x, y);
+        drawCircle(x2, y2);
+        drawLine(x, y, x2, y2);
+        x = x2;
+        y = y2;
     }
 });
 
@@ -36,8 +48,17 @@ function drawCircle(x,y){
     ctx.fill();
 }
 
+function drawLine(x1, y1, x2, y2){
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.strokeStyle = color;
+    ctx.lineWidth = size * 2;
+    ctx.stroke();
+}
+
 increaseBtn.addEventListener('click',()=>{
-    size +=5;
+    size +=2.5;
     if (size>50){
         size=50;
     }
@@ -45,11 +66,15 @@ increaseBtn.addEventListener('click',()=>{
 });
 
 decreaseBtn.addEventListener('click',()=>{
-    size -=5;
+    size -=2.5;
     if (size<5){
         size=5;
     }
     updateBrushSize();
+});
+
+clearBtn.addEventListener('click', ()=>{
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
 
 colorElem.addEventListener('change',(e)=>{
